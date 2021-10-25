1999722ae859fdd918f88e449b539cbcca4cdb0f
Maxime Lecomte <mystelo0970@gmail.com>
2021-10-25 12:52:45 +0200

    enregistrement jour 1 avant manger

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,35 @@
 var express = require('express');
 var router = express.Router();
 
+var weatherCard =
+[
+  {cityName: "Paris", weatherReport : "nuageux", pictureClass: "fas fa-cloud-showers-heavy", minTemp: 15, maxTemp: 30},
+  {cityName: "Marseille", weatherReport : "ensoleilé", pictureClass: "fas fa-sun", minTemp: 15, maxTemp: 30}
+]
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+  res.render('index');
 });
+
+
+router.post('/weather', function(req, res, next) {
+  console.log(weatherCard)
+  res.render('weather',{data:weatherCard});
+});
+
+router.get('/deconnexion', function(req, res, next) {
+  res.render('index');
+});
+
+
+
+
+
+
+
+
+
 
 module.exports = router;
